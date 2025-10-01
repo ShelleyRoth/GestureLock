de00ddc27afd867bb82338be12f473964bb44e33
Shelley Roth <rothshelley@outlook.com>
2025-10-01 17:07:06 +0300

    Milestone 1 - Ultrasonic Distance Reading & Distance Category Detection & Button

--- GestureLock.ino
+++ GestureLock.ino
@@ -15,13 +15,23 @@
 #define DHT_PIN 7 // DHT-11 Output Pin connection
 #define DHT_TYPE DHT11 // DHT type is DHT-11
 
-// // Hook up HC-SR04 
+// Hook up HC-SR04 
 #define TRIGGER_PIN 9 // Trig to Arudino pin 9,
 #define ECHO_PIN 10 //  Echo to Arduino pin 10
 #define MAX_DISTANCE 400 //
 
-// pinMode(TRIGGER_PIN, OUTPUT);
-// pinMode(ECHO_PIN, INPUT);
+// Thresholds for Distance Definitions
+#define NEAR 10 // An object's distance x will be defined as near if x <= 10 cm
+#define MEDIUM 20 // An object's distance x will be defined as medium if 10 < x <= 20
+// An object's distance x will be defined as far if x > 20
+
+// Integer defintion of NEAR, MEDIUM, and FAR
+#define INT_NEAR 0
+#define INT_MEDIUM 1
+#define INT_FAR 2
+
+// Button
+#define BUTTON_PIN 2 // digital pin connected to the button
 
 // Initialize DHT sensor
 DHT dht(DHT_PIN, DHT_TYPE);
@@ -39,19 +49,20 @@ float speedOfSoundMpS; // Stores calculated speed of sound in meters per second
 float speedOfSoundCMpMS; // Stores calculated speed of sound in cm per microsecond
 int iterations = 5; // to calculate average duration from a given object and stabalize output readings
 
+int correctPasscode[3] = {0, 1, 2};
+int userPasscode[3] = {};
+
 void setup() {
   // put your setup code here, to run once:
 
   //Initialize Serial Monitor
   Serial.begin(9600); // begin communicating at 9600 bits per second
   dht.begin(); // intialize temp and humidity sensor
+  pinMode(BUTTON_PIN, INPUT);
 
 }
 
-void loop() {
-
-
-// sketch version 2 - more accurate!
+float calculateDistance() {
 
 delay(2000); // delay so DHT sensor has time to stabalize
 
@@ -73,8 +84,7 @@ duration = sonar.ping_median(iterations); // to get the average duration
 distance = speedOfSoundCMpMS * (duration / 2); // divide duration by 2 because the duration is calculated as a "round trip"
 
 
-
-
+// Print details of speed of sound in m/s, humidity percentage, temperature in Celcius, and distance in cm
 Serial.print("Sound: ");
 Serial.print(speedOfSoundMpS);
 Serial.print(" m/s, "); 
@@ -91,10 +101,44 @@ if(distance >= 400 || distance <= 2) {
   Serial.println("OUT OF RANGE");
 } else {
   Serial.print(distance);
-  Serial.println(" cm");
+  Serial.print(" cm");
+  if(distance <= NEAR) {
+    Serial.println("NEAR");
+  } else if(distance <= MEDIUM) {
+    Serial.println("Medium");
+  } else {
+    Serial.println("FAR");
+  }
   delay(500);
 }
 delay(500);
+
+return distance;
+}
+
+bool isButtonPressed() {
+  int buttonState = digitalRead(BUTTON_PIN);
+  // check if button is pressed
+  if(buttonState == LOW) {
+    // debounce delay
+    delay(50);
+    //reread to confirm press after debounce
+    if(digitalRead(BUTTON_PIN) == LOW) {
+      // digitalWrite(LED_PIN, HIGH); // Turn LED on
+      Serial.println("Button not Pressed");
+    }
+  } else {
+    // digitalWrite(LED_PIN, LOW); // turn LED off
+    Serial.println("Button Pressed");
+  }
+}
+
+
+void loop() {
+  int distance = calculateDistance();
+  isButtonPressed();
+
+
 
 
 }
